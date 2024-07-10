00e5633ad05da06e3fe15a89f587c2149f9cde18
Julio1230 <julio.ledezma_olivares@colostate.edu>
2024-07-09 21:02:07 -0600

    nice

---
+++ src/BubbleSort.js
@@ -0,0 +1,10 @@
+const BubbleSort = () => {
+    return(
+        <div className="bubblesort">
+            <h1>Bubble Sort</h1>
+            <h2>Introduction</h2>
+        </div>
+    );
+}
+
+export default BubbleSort;
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import './home.css'
+
+function Home(){
+    return(
+        <>
+        <h1>
+            Algorithms Visual
+        </h1><p>
+                This is a website dedicated to visualizing algorithms for educational purposes.
+                The goal is to visualize as many algorithms as possible and help students learn
+                these topics through visuals. There are many types of algorithms that we will discuss
+                so we deicded to group them click on an algorithm you would like to learn more about from the options below.
+            </p><h2>Sorting Algorithms</h2><p> Sorthing algorithms are some of the first algorithms that are taught when you first start
+                your computer science journey. Although some of these algorithms are not used in real world applications it
+                is good to way to adapt a new form a thinking. In computer science there is use of arrays and getting a basic understanding
+                of indexes and and index values is a perfect place to start.
+            </p><h4>Bubble Sort Insertion Sort</h4>
+        </>
+    )
+}
+
+export default Home;
